e0059a91f308cb6e65af5a55cddbf6c121d3f2eb
Nick Bohlen <mail@bohlen-design.de>
2012-02-14 10:06:54 +0100

    added index.php

---
+++ index.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+	<title></title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<meta name="viewport" content="width=device-width">
+  
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="css/grid.css">
+	<link rel="stylesheet" href="css/10-jahre.css">
+
+	<script src="js/libs/modernizr-2.5.2.min.js"></script>
+</head>
+<body>
+<!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
+
+  <div id="content" class="container_12">
+
+    <?php include "includes/html_header.htm" ?>
+    <!-- header --><?php include "includes/header.htm" ?>
+    <!-- teaser --><?php include "includes/teaser.htm" ?>
+    <!-- so_stehts_mit_button --><?php include "includes/so_stehts_mit_button.htm" ?>
+    <!-- social_log --><?php include "includes/social_log.htm" ?>
+    <!-- so_gehts --><?php include "includes/so_gehts.htm" ?>
+    <!-- timeline_teaser --><?php include "includes/timeline_teaser.htm" ?>
+    <!-- blog_roll --><?php include "includes/blog_roll.htm" ?>
+    <!-- schniepel --><?php include "includes/schniepel.htm" ?>
+    <!-- your friends --><?php include "includes/your_friends.htm" ?>
+    <!-- footer --><?php include "includes/footer.htm" ?>
+    
+    
+  </div>
+  <!-- footer -->
+  
+  <div id="footer">
+    <div class="container_12">
+      <div class="part1">
+          Folge uns auf:
+      </div>
+      <div class="part2">
+        <ul>
+          <li><a href="blog.htm">Blog</a></li>
+          <li><a href="blog.htm">Die Aktion</a></li>
+          <li><a href="blog.htm">Aktuelles Voting</a></li>
+          <li><a href="blog.htm">Votings-Archiv</a></li>
+          <li><a href="blog.htm">Mein Konto</a></li>
+        </ul>
+        <ul>
+          <li><a href="blog.htm">Über uns</a></li>
+          <li><a href="blog.htm">Presse</a></li>
+          <li><a href="blog.htm">FAQ</a></li>
+          <li><a href="blog.htm">Feedback / Kontakt</a></li>
+          <li><a href="blog.htm">Datenschutz</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+</body>
+</html>
+
+
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/libs/jquery-1.7.1.min.js"><\/script>')</script>
+
+<script src="js/script.js"></script>
+
+</body>
+</html>
