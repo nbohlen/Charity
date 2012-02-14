8a35438a662c78f622f18de13c49d5f77cf8961a
Nick Bohlen <mail@bohlen-design.de>
2012-02-14 10:24:19 +0100

    choices.php

---
+++ choice.php
@@ -0,0 +1,8 @@
+<?php include  "includes/html_header.htm" ?>
+<!-- header --><?php include  "includes/header.htm" ?>
+<!-- month --><?php include "includes/month.htm" ?>
+<!-- 5_choices --><?php include  "includes/5_choices.htm" ?>
+<!-- schniepel --><?php include  "includes/schniepel.htm" ?>
+<!-- so_stehts --><?php include  "includes/so_stehts.htm" ?>
+<!-- choice_popup --><?php include  "includes/choice_popup.htm" ?>
+<!-- footer --><?php include "includes/footer.htm" ?>
