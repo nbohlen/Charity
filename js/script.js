242a19d2c0192f063dc8592aca520b8b49d68e06
Nick Bohlen <mail@bohlen-design.de>
2012-02-14 18:34:19 +0100

    details

--- js/script.js
+++ js/script.js
@@ -2,6 +2,14 @@
 
 */
 
+$(document).ready(function() {
+  $('#so_gehts ul li').click(function(){
+    $('#so_gehts').removeClass('first second third')
+    $('#so_gehts').addClass($(this).attr('class'));
+  })
+});
+
+
 
 
 
