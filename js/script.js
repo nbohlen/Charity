44f01d94f468578646c5ae622d0711c0a065fe4a
Nick Bohlen <mail@bohlen-design.de>
2012-02-27 14:35:14 +0100

    so gehts hinzugefügt

--- js/script.js
+++ js/script.js
@@ -4,8 +4,11 @@
 
 $(document).ready(function() {
   $('#so_gehts ul li').click(function(){
+    var myclass = $(this).attr('class');
+    $('#so_gehts .pane.active').removeClass('active');
+    $('#so_gehts').find('#' + myclass ).addClass('active');
     $('#so_gehts').removeClass('first second third')
-    $('#so_gehts').addClass($(this).attr('class'));
+    $('#so_gehts').addClass(myclass);
   })
 });
 
