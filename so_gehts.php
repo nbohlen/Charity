8f9cc411260c387d80eba05ccb0e484e52bf7275
Nick Bohlen <mail@bohlen-design.de>
2012-02-29 17:40:24 +0100

    change red_button position

---
+++ so_gehts.php
@@ -0,0 +1,4 @@
+<?php include "includes/html_header.htm" ?>
+<!-- so_gehts --><?php include "includes/so_gehts_large.htm" ?>
+<!-- schniepel --><?php include "includes/schniepel.htm" ?>
+<!-- footer --><?php include "includes/footer.htm" ?>
