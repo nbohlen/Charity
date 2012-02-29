3c48f007540291b3a1497b263bb5850fbf7a166c
Nick Bohlen <mail@bohlen-design.de>
2012-02-29 16:15:32 +0100

    details

--- index.php
+++ index.php
@@ -4,8 +4,5 @@
 <!-- so_stehts_mit_button --><?php include "includes/so_stehts_mit_button.htm" ?>
 <!-- social_log --><?php include "includes/social_log.htm" ?>
 <!-- so_gehts --><?php include "includes/so_gehts.htm" ?>
-<!-- timeline_teaser --><?php include "includes/timeline_teaser.htm" ?>
-<!-- blog_roll --><?php include "includes/blog_roll.htm" ?>
 <!-- schniepel --><?php include "includes/schniepel.htm" ?>
-<!-- your friends --><?php include "includes/your_friends.htm" ?>
 <!-- footer --><?php include "includes/footer.htm" ?>
