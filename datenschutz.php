ed1809a714c40bff378e49a1c07f63aa34f40421
Nick Bohlen <mail@bohlen-design.de>
2012-02-29 11:50:03 +0100

    added static

---
+++ datenschutz.php
@@ -0,0 +1,5 @@
+<?php include "includes/html_header.htm" ?>
+<!-- header --><?php include "includes/header.htm" ?>
+<!-- so_gehts --><?php include "includes/datenschutz.htm" ?>
+<!-- schniepel --><?php include "includes/schniepel.htm" ?>
+<!-- footer --><?php include "includes/footer.htm" ?>
---
+++ login_popup.php
@@ -0,0 +1,117 @@
+<div id="register_login">
+    <div class="method shopapo wide">
+      <div class="form"><style>
+  #shopapo_login_form { font-size: 10px;}
+  #shopapo_login_form div { margin: 10px 0px 0px 0px; padding: 0; height: 32px; width: 100% }
+  #shopapo_login_form label {
+    display: block; width: 80px; height: 100%; margin: 0px 10px 0px 0px; float: left; }
+  #shopapo_login_form input,
+  #shopapo_login_form button, 
+  #shopapo_login_form a {
+    display: block; width: 242px; height: 100%; margin: 0px; float: right; }
+  #shopapo_login_form button {
+    border: none; background-position: 0px -297px }  
+  #shopapo_login_form button:hover {
+    background-position: 0px -330px }
+  #shopapo_login_form button:active {
+    background-position: 0px -363px }
+    
+</style>
+<form action="https://www.shop-apotheke.com/10-jahre/shopapo_login.go" id="shopapo_login_form" name="login" method="post">
+<input type="hidden" name="fromtemplate" value="/10-jahre/datenschutz.htm">
+    <p>Verwendenen Sie Ihre www.shop-apotheke.com Kundendaten, um abzustimmen</p>
+    <div>
+        <label for="shopapo_login">Benutzername:</label>
+        <input id="shopapo_login" type="text" name="login">
+    </div>
+    <div>
+        <label for="shopapo_password">Passwort:</label>
+        <input id="shopapo_password" type="password" name="password">
+    </div>
+    <div>
+        <button class="button" type="submit">Jetzt abstimmen!</button>
+    </div>
+    <div>
+        <a href="http://www.shop-apotheke.com/pw_vergessen.htm">Passwort vergessen</a>
+    </div>
+</form>    </div>
+      <div class="chooser">
+          <div class="logo">&nbsp;</div>
+          <div class="label anmelden">Anmelden mit shop-apotheke.com Kundenkonto</div>
+          <div class="label abstimmen">Abstimmen mit shop-apotheke.com Kundenkonto</div>
+      </div>
+    </div>
+    <div class="method facebook wide">
+      <div class="form"><iframe src="https://www.facebook.com/plugins/registration.php?client_id=&amp;redirect_uri=https%3A%2F%2Fext-shopapotheke.dev.xsite.de%2F10-jahre%2Fconnect.go&amp;fields=name,email" scrolling="auto" frameborder="no" style="border:none" allowtransparency="true" width="440" height="317">
+</iframe>
+<!--      <div id="fb-root"></div>
+      <script>
+        window.fbAsyncInit = function() {
+          FB.init({
+            appId      : '',
+            status     : true, 
+            cookie     : true,
+            xfbml      : true,
+            oauth      : true,
+          });
+        };
+        (function(d){
+           var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
+           js = d.createElement('script'); js.id = id; js.async = true;
+           js.src = "//connect.facebook.net/en_US/all.js";
+           d.getElementsByTagName('head')[0].appendChild(js);
+         }(document));
+      </script>
+      <div class="fb-login-button" data-scope="email,user_checkins">
+        Login with Facebook
+      </div> -->
+</div>
+      <div class="chooser">
+          <div class="logo">&nbsp;</div> 
+          <div class="label anmelden">Anmelden mit facebook Benutzerkonto</div>
+          <div class="label abstimmen">Abstimmen mit facebook Benutzerkonto</div>
+      </div>
+    </div>
+    <div class="method charity wide">
+      <div class="form"><style>
+  #shopapo_login_form { font-size: 10px;}
+  #shopapo_login_form div { margin: 10px 0px 0px 0px; padding: 0; height: 32px; width: 100% }
+  #shopapo_login_form label {
+    display: block; width: 80px; height: 100%; margin: 0px 10px 0px 0px; float: left; }
+  #shopapo_login_form input,
+  #shopapo_login_form button, 
+  #shopapo_login_form a {
+    display: block; width: 242px; height: 100%; margin: 0px; float: right; }
+  #shopapo_login_form button {
+    border: none; background-position: 0px -297px }  
+  #shopapo_login_form button:hover {
+    background-position: 0px -330px }
+  #shopapo_login_form button:active {
+    background-position: 0px -363px }
+    
+</style>
+<form action="https://www.shop-apotheke.com/10-jahre/shopapo_login.go" id="shopapo_login_form" name="login" method="post">
+<input type="hidden" name="fromtemplate" value="/10-jahre/datenschutz.htm">
+    <p>Melden Sie sich mit ihrer Email-Adresse an</p>
+    <div>
+        <label for="shopapo_login">Benutzername:</label>
+        <input id="shopapo_login" type="text" name="login">
+    </div>
+    <div>
+        <label for="shopapo_password">Passwort:</label>
+        <input id="shopapo_password" type="password" name="password">
+    </div>
+    <div>
+        <button class="button" type="submit">Jetzt abstimmen!</button>
+    </div>
+    <div>
+        <a href="http://www.shop-apotheke.com/pw_vergessen.htm">Passwort vergessen</a>
+    </div>
+</form>    </div>
+      <div class="chooser">
+          <div class="logo">&nbsp;</div>
+          <div class="label anmelden">Anmelden mit Email-Adresse</div>
+          <div class="label abstimmen">Abstimmen mit Email-Adresse</div>
+      </div>
+     </div>
+  </div>
